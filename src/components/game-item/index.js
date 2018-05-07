ed6c5f66dae18acad49161e187b0abf7f741ab1f
Brian Bixby <brianbixby0@gmail.com>
2018-05-06 17:40:36 -0700

    added animations to game pickS

--- src/components/game-item/index.js
+++ src/components/game-item/index.js
@@ -1,27 +1,39 @@
 import React from 'react';
+import * as util from './../../lib/util.js';
 
 class GameItem extends React.Component {
   constructor(props){
     super(props);
-    this.state = {gameID: props.game._id, gameTime: props.game.dateTime, pick: '', awayTeam: props.game.awayTeam._id, homeTeam: props.game.homeTeam._id};
+    this.state = {gameID: props.game._id, gameTime: props.game.dateTime, pick: '', pickName: '', awayTeam: props.game.awayTeam._id, homeTeam: props.game.homeTeam._id};
   }
 
   awayTeamPick = team => {
-    this.setState({ pick: team });
-    return this.props.onComplete({gameID: this.state.gameID, gameTime: this.state.gameTime, pick: this.state.awayTeam});
+    this.setState({ pick: team, pickName: this.props.game.awayTeam.teamName });
+    setTimeout(() => {
+      this.props.onComplete({gameID: this.state.gameID, gameTime: this.state.gameTime, pick: this.state.awayTeam})
+    }, 2500);
   };
 
   homeTeamPick = team => {
-    return this.props.onComplete({gameID: this.state.gameID, gameTime: this.state.gameTime, pick: this.state.homeTeam});
+    this.setState({ pick: team, pickName: this.props.game.homeTeam.teamName});
+    setTimeout(() => {
+      this.props.onComplete({gameID: this.state.gameID, gameTime: this.state.gameTime, pick: this.state.homeTeam})
+    }, 2500);
   };
   
   render() {
     let { game } = this.props;
     let gameDetailsStyle = {
       background: `linear-gradient(to right, ${game.homeTeam.color}, ${game.awayTeam.color})`,
-    }
+    };
+    let homeBorderStyle = {
+      background: `${game.homeTeam.color}`,
+    };
+    let awayBorderStyle = {
+      background: `${game.awayTeam.color}`,
+    };
     return (
-      <div className='container'>
+      <div className={ this.state.pickName ? 'container fadeout' : 'container' }>
         <div className='eventDetails gameDetails' style={gameDetailsStyle}>
           <p className='eventNote'> 
             {game.homeTeam.teamCity} vs {game.awayTeam.teamCity}
@@ -69,11 +81,11 @@ class GameItem extends React.Component {
               <div className='starPlayerImageDiv homeStarPlayerImageDiv'>
                 <div className='starPlayerImageDivWrapper'>
                   <div className='starPlayerImageDivInnerWrapper'>
-                    <div className='playerCardOuter'>
+                    <div className='playerCardOuter' onClick={this.homeTeamPick}>
                       <div className='playerCardImageWrapper'>
                         <img className='starPlayerImages' src={game.homeTeam.starPlayerImage}/>
                       </div>
-                      <div className='playerCardContentBorderTop'></div>
+                      <div className='playerCardContentBorderTop' style={homeBorderStyle}></div>
                       <div className='playerCardNameWrapper'>
                         <p className='starPlayerName'>{game.homeTeam.starPlayer}</p>
                       </div>
@@ -87,11 +99,11 @@ class GameItem extends React.Component {
               <div className='starPlayerImageDiv awayStarPlayerImageDiv'>
                 <div className='starPlayerImageDivWrapper'>
                   <div className='starPlayerImageDivInnerWrapper'>
-                    <div className='playerCardOuter'>
+                    <div className='playerCardOuter' onClick={this.awayTeamPick}>
                       <div className='playerCardImageWrapper'>
                         <img className='starPlayerImages' src={game.awayTeam.starPlayerImage}/>
                       </div>
-                      <div className='playerCardContentBorderTop'></div>
+                      <div className='playerCardContentBorderTop' style={awayBorderStyle}></div>
                       <div className='playerCardNameWrapper'>
                         <p className='starPlayerName'>{game.awayTeam.starPlayer}</p>
                       </div>
@@ -102,6 +114,12 @@ class GameItem extends React.Component {
             </div>
           </div>
         </div>
+        {util.renderIf(this.state.pick,
+          <p className='checkmarkDiv'>
+            <span className='gamePick'>{this.state.pickName}</span>
+            <svg className="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52"><circle className="checkmark__circle" cx="26" cy="26" r="25" fill="none"/><path className="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8"/></svg>
+          </p>
+        )}
       </div>
     );
   }
--- src/components/user-pick-item/index.js
+++ src/components/user-pick-item/index.js
@@ -13,6 +13,16 @@ class UserPickItem extends React.Component {
       homeTeamName: props.userPick.gameID.homeTeam.teamName,
       homeTeamWins: props.userPick.gameID.homeTeam.wins,
       homeTeamLosses: props.userPick.gameID.homeTeam.losses,
+      awayTeamCity: props.userPick.gameID.awayTeam.teamCity,
+      awayTeamStarPlayer: props.userPick.gameID.awayTeam.starPlayer,
+      awayTeamStarPlayerImage: props.userPick.gameID.awayTeam.starPlayerImage,
+      awayTeamImage: props.userPick.gameID.awayTeam.image,
+      awayTeamColor: props.userPick.gameID.awayTeam.color,
+      homeTeamCity: props.userPick.gameID.homeTeam.teamCity,
+      homeTeamStarPlayer: props.userPick.gameID.homeTeam.starPlayer,
+      homeTeamStarPlayerImage: props.userPick.gameID.homeTeam.starPlayerImage,
+      homeTeamImage: props.userPick.gameID.homeTeam.image,
+      homeTeamColor: props.userPick.gameID.homeTeam.color,
       editing: false}
   }
 
@@ -22,13 +32,17 @@ class UserPickItem extends React.Component {
   }
 
   awayTeamPickUpdate = team => {
-    this.setState({editing: false});
-    return this.props.onUpdate({ _id: this.state._id, pick: this.state.awayTeamID });
+    if(this.state.editing) {
+      this.setState({editing: false});
+      return this.props.onUpdate({ _id: this.state._id, pick: this.state.awayTeamID });
+    }
   };
 
   homeTeamPickUpdate = team => {
-    this.setState({editing: false});
-    return this.props.onUpdate({ _id: this.state._id, pick: this.state.homeTeamID });
+    if(this.state.editing) {
+      this.setState({editing: false});
+      return this.props.onUpdate({ _id: this.state._id, pick: this.state.homeTeamID });
+    }
   };
 
   handleEdit = () => {
@@ -39,31 +53,126 @@ class UserPickItem extends React.Component {
     let { userPick } = this.props;
     let { editing } = this.state;
     let currentPick = userPick.pick === this.state.homeTeamID ? this.state.homeTeamName : this.state.awayTeamName;
-    return (
-      <div className='userPickItem'>
-        <div className='gawayTeamDiv'>
-          {util.renderIf(editing, 
-            <button className='teamName teamNameButton' onClick={this.awayTeamPickUpdate}>{this.state.awayTeamName}</button>
-          )}
-          {util.renderIf(!editing, 
-            <span className='teamName teamNameButton'>{this.state.awayTeamName}</span>
-          )}
-          <p className='teamRecord'>{this.state.awayTeamWins} - {this.state.awayTeamLosses}</p>
-        </div>
-        <span className='game-dateTime'>{new Date(userPick.gameTime).toDateString()}</span>
-        <span className='currentPick'>current pick {currentPick}</span>
-        <div className='homeTeamDiv'>
-          {util.renderIf(editing, 
-            <button className='teamName teamNameButton' onClick={this.homeTeamPickUpdate}>{this.state.homeTeamName}</button>
-          )}
-          {util.renderIf(!editing, 
-            <span className='teamName teamNameButton'>{this.state.homeTeamName}</span>
-          )}
-          <p className='teamRecord'>{this.state.homeTeamWins} - {this.state.homeTeamLosses}</p>
 
-          <p className='editingButton' onClick={this.handleEdit}><i className="fa fa-edit"></i></p>
+    let gameDetailsStyle = {
+      background: `linear-gradient(to right, ${this.state.homeTeamColor}, ${this.state.awayTeamColor})`,
+    };
+    let homeBorderStyle = {
+      background: `${this.state.homeTeamColor}`,
+    };
+    let awayBorderStyle = {
+      background: `${this.state.awayTeamColor}`,
+    };
+    return (
+      // <div className='userPickItem'>
+      //   <div className='gawayTeamDiv'>
+      //     {util.renderIf(editing, 
+      //       <button className='teamName teamNameButton' onClick={this.awayTeamPickUpdate}>{this.state.awayTeamName}</button>
+      //     )}
+      //     {util.renderIf(!editing, 
+      //       <span className='teamName teamNameButton'>{this.state.awayTeamName}</span>
+      //     )}
+      //     <p className='teamRecord'>{this.state.awayTeamWins} - {this.state.awayTeamLosses}</p>
+      //   </div>
+      //   <span className='game-dateTime'>{new Date(userPick.gameTime).toDateString()}</span>
+      //   <span className='currentPick'>current pick {currentPick}</span>
+      //   <div className='homeTeamDiv'>
+      //     {util.renderIf(editing, 
+      //       <button className='teamName teamNameButton' onClick={this.homeTeamPickUpdate}>{this.state.homeTeamName}</button>
+      //     )}
+      //     {util.renderIf(!editing, 
+      //       <span className='teamName teamNameButton'>{this.state.homeTeamName}</span>
+      //     )}
+      //     <p className='teamRecord'>{this.state.homeTeamWins} - {this.state.homeTeamLosses}</p>
+
+      //     <p className='editingButton' onClick={this.handleEdit}><i className="fa fa-edit"></i></p>
+      //   </div>
+      // </div>
+
+<div className='container'>
+  <div className='eventDetails gameDetails' style={gameDetailsStyle}>
+    <p className='eventNote'> 
+      {this.state.awayTeamCity} vs {this.state.homeTeamCity}
+    </p>
+  </div>
+  <div className='gameContent'>
+    <div className='homeTeamContent'  onClick={this.homeTeamPickUpdate}>
+      <div className='teamContainer'>
+        <div className='teamContentWrapper'>
+          <div className='teamInfo'>
+            <div className='teamInfoWrapper'>
+              <p className='homeTeamName'>{this.state.homeTeamName}</p>
+              <p className='homeTeamRecord'><span>{this.state.homeTeamWins} - {this.state.homeTeamLosses}</span></p>
+            </div>
+          </div>
+          <div className='gameLogoWrapper'>
+            <img className='homeTeamLogo gameLogo' src={this.state.homeTeamImage} />
+          </div>
         </div>
       </div>
+    </div>
+    <div className='gameDate'>
+      <p className='gameDateP'>{new Date(userPick.gameTime).toDateString()}</p>
+    </div>
+    <div className='awayTeamContent' onClick={this.awayTeamPickUpdate}>
+      <div className='teamContainer'>
+        <div className='teamContentWrapper'>
+          <div className='gameLogoWrapper'>
+            <img className='awayTeamLogo gameLogo' src={this.state.awayTeamImage} />
+          </div>
+          <div className='teamInfo'>
+            <div className='teamInfoWrapper'>
+              <p className='awayTeamName'>{this.state.awayTeamName}</p>
+              <p className='awayTeamRecord'><span>{this.state.awayTeamWins} - {this.state.awayTeamLosses}</span></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div className='createMain'>
+    <div className='createMainWrapper playerDiv'>
+      <div className='createImgDiv'>
+        <div className='starPlayerImageDiv homeStarPlayerImageDiv'>
+          <div className='starPlayerImageDivWrapper'>
+            <div className='starPlayerImageDivInnerWrapper'>
+              <div className='playerCardOuter' onClick={this.homeTeamPickUpdate}>
+                <div className='playerCardImageWrapper'>
+                  <img className='starPlayerImages' src={this.state.homeTeamStarPlayerImage}/>
+                </div>
+                <div className='playerCardContentBorderTop' style={homeBorderStyle}></div>
+                <div className='playerCardNameWrapper'>
+                  <p className='starPlayerName'>{this.state.homeTeamStarPlayer}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className='vsDiv'>
+          <p className='vs'>vs</p>
+        </div>
+        <div className='starPlayerImageDiv awayStarPlayerImageDiv'>
+          <div className='starPlayerImageDivWrapper'>
+            <div className='starPlayerImageDivInnerWrapper'>
+              <div className='playerCardOuter' onClick={this.awayTeamPickUpdate}>
+                <div className='playerCardImageWrapper'>
+                  <img className='starPlayerImages' src={this.state.awayTeamStarPlayerImage}/>
+                </div>
+                <div className='playerCardContentBorderTop' style={awayBorderStyle}></div>
+                <div className='playerCardNameWrapper'>
+                  <p className='starPlayerName'>{this.state.awayTeamStarPlayer}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <p className='editingButton' onClick={this.handleEdit}><i className="fa fa-edit"></i></p>
+    <p className='currentPick'>current pick {currentPick}</p>
+  </div>
+</div>
     );
   }
 }
