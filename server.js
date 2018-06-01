3f14ee44a2a63d88e1534a04cf5f9c96375428d3
Brian Bixby <brianbixby0@gmail.com>
2018-05-31 23:40:50 -0700

    figured it out

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(cors());
-app.use(express.static(`${__dirname}/build`));
+app.use(express.static(`${__dirname}/dist`));
 
 app.listen(PORT, () => {
   console.log(`listening on ${PORT}`);
