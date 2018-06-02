9c4719d4e636254a3037693d8c3446574fe77722
Brian Bixby <brianbixby0@gmail.com>
2018-06-01 17:54:36 -0700

    banner ad is good for now

--- src/components/helpers/bannerAd/index.js
+++ src/components/helpers/bannerAd/index.js
@@ -3,7 +3,10 @@ import React from 'react';
 class BannerAd extends React.Component {
   render() {
     return (
-      <p className='bannerAd' onClick={()=> window.open('https://www.espn.com', '_blank')}>text</p>
+      <p className='bannerAd' onClick={()=> window.open('https://www.espn.com', '_blank')}>
+        <span className='company'>BUILT BY BIXBY</span>
+        <span className='tagLine'>LIFE NEEDS WEBSITES</span>
+      </p>
     );
   }
 }
